0e41a419cbb397fa92b4a20120ec89a351cc41c1
hapm <ha.con.bk@gmail.com>
2021-03-24 16:51:24 +0900

    u

--- scroll.js
+++ scroll.js
@@ -9,17 +9,16 @@ function openLeftMenu() {
 }
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
-
 // Get the navbar
 var sidebar = document.getElementById("mySideBar");
-
+var leftmenubar = document.getElementById("myLeftSideBar");
 // Get the offset position of the navbar
 var sticky = sidebar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
-    sidebar.classList.add("sticky")
+    sidebar.classList.add("sticky");
   } else {
     sidebar.classList.remove("sticky");
   }
